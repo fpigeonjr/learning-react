524046f5c0104a64f2fc39fdb7d9f8a50e666c1c
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-09-08 13:53:40 -0500

    corrects bugs and typos

--- ch05/src/components/Recipe.js
+++ ch05/src/components/Recipe.js
@@ -4,7 +4,7 @@ import Instructions from './Instructions'
 
 function Recipe({ name, ingredients, steps }) {
   return (
-    <section id={name.toLowercase().replace(/ /g, '-')}>
+    <section id={name.toLowerCase().replace(/ /g, '-')}>
       <h1>{name}</h1>
       <IngredientsList list={ingredients} />
       <Instructions title="Cooking Instructions" steps={steps} />
