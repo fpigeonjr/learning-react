71b480968759b1956000d5f5c5a570e44aeeb94b
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-09-08 18:33:19 -0500

    starts color form

---
+++ ch06/src/AddColorForm.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function AddColorForm(onNewColor = (f) => f) {
+  const txtTitle = React.useRef()
+  const hexColor = React.useRef()
+  const submit = (e) => {
+    e.preventDefault()
+    const title = txtTitle.current.value
+    const color = hexColor.current.value
+    onNewColor(title, color)
+    txtTitle.current.value = ''
+    hexColor.current.value = ''
+  }
+
+  return (
+    <form onSubmit={submit}>
+      <input ref={txtTitle} type="text" placeholder="color title" required />
+      <input ref={hexColor} type="color" required />
+      <button>ADD</button>
+    </form>
+  )
+}
+
+export default AddColorForm
