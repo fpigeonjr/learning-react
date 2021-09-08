efb4a21c4f3e6fa4889fbdc9bfd4a1222177454d
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-09-08 17:55:28 -0500

    adds  for removal of Color

--- ch06/src/App.js
+++ ch06/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import StarRating from './StarRating'
 import colorData from './color-data.json'
 import ColorList from './ColorList'
 
 function App() {
-  const [colors, SetColors] = React.useState(colorData)
-  return (
-    // <StarRating
-    //   style={{ backgroundColor: 'lightblue' }}
-    //   onDoubleClick={(e) => alert('double click')}
-    // />
-    <ColorList colors={colors} />
-  )
+  const [colors, setColors] = React.useState(colorData)
+
+  // const removeColor = (id) => {
+  //   const newColors = colors.filter((color) => color.id !== id)
+  //   SetColors(newColors)
+  // }
+
+  const removeColor = (id) => {
+    const newColors = colors.filter((color) => color.id !== id)
+    setColors(newColors)
+  }
+
+  return <ColorList colors={colors} onRemoveColor={removeColor} />
 }
 
 export default App
--- ch06/src/ColorList.js
+++ ch06/src/ColorList.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Color from './Color'
 
-function ColorList({ colors = [] }) {
+function ColorList({ colors = [], onRemoveColor = (f) => f }) {
   if (!colors.length) return <div>No Colors Listed.</div>
   return (
     <div>
       {colors.map((color, i) => (
-        <Color key={color.id} {...color} />
+        <Color key={color.id} {...color} onRemove={onRemoveColor} />
       ))}
     </div>
   )
