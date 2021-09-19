6e5e8bed7f604064710a53b8f471c6a0f393eab9
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-09-19 14:34:31 -0500

    useReducer example

--- ch07/src/App.js
+++ ch07/src/App.js
@@ -51,8 +51,48 @@ function WordCount({ children = '' }) {
   )
 }
 
+const firstUser = {
+  id: '1230123-123',
+  firstName: 'Bill',
+  lastName: 'Wilson',
+  city: 'Missoula',
+  state: 'Montana',
+  email: 'bwilson@mtnwilson.com',
+  admin: false,
+}
+
+function User() {
+  const [user, setUser] = React.useReducer(
+    (user, newDetails) => ({
+      ...user,
+      ...newDetails,
+    }),
+    firstUser
+  )
+
+  return (
+    <div>
+      <h1>
+        {user.firstName} {user.lastName}
+      </h1>
+      <p>Email: {user.email}</p>
+      <p>
+        Location: {user.city}, {user.state}
+      </p>
+      <p>Role: {user.admin ? 'admin' : 'user'}</p>
+      <button
+        onClick={() => {
+          setUser({ admin: true })
+        }}
+      >
+        Make Admin
+      </button>
+    </div>
+  )
+}
+
 function App() {
-  return <WordCount>You are not going to believe this but...</WordCount>
+  return <User />
 }
 
 export default App
