20a4b2afb2b8610af01726ea07ebed1e2bf4287b
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-09-26 16:57:29 -0500

    memoises values

--- ch08/src/useIterator.js
+++ ch08/src/useIterator.js
@@ -3,15 +3,17 @@ import React from 'react'
 export const useIterator = (items = [], initialIndex = 0) => {
   const [i, setIndex] = React.useState(initialIndex)
 
-  const prev = () => {
+  const prev = React.useCallback(() => {
     if (i === 0) return setIndex(items.length - 1)
     setIndex(i - 1)
-  }
+  }, [i])
 
-  const next = () => {
+  const next = React.useCallback(() => {
     if (i === items.length - 1) return setIndex(0)
     setIndex(i + 1)
-  }
+  }, [i])
 
-  return [items[i], prev, next]
+  const item = React.useMemo(() => items[i], [i])
+
+  return [item || items[0], prev, next]
 }
