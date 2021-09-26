a65b13e5bd77d9d2fc337ae811ca321efb7bdfe6
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2021-09-26 14:40:42 -0500

    starts render props

--- ch08/src/App.js
+++ ch08/src/App.js
@@ -34,6 +34,39 @@ function GitHubUser({ login }) {
   )
 }
 
+// render props
+
+const tahoe_peaks = [
+  {
+    name: 'Feel Free',
+    elevation: 10891,
+  },
+  {
+    name: 'Mountain Peak',
+    elevation: 10067,
+  },
+  {
+    name: 'Pyramid Peak',
+    elevation: 9983,
+  },
+  {
+    name: 'Mt. Tallac',
+    elevation: 9735,
+  },
+]
+
 export default function App() {
-  return <GitHubUser login="fpigeonjr" />
+  return (
+    <>
+      <GitHubUser login="fpigeonjr" />
+      <h2>Tahoe Peaks</h2>
+      <ul>
+        {tahoe_peaks.map((peak, i) => (
+          <li key={i}>
+            {peak.name} - {peak.elevation.toLocaleString()}ft
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
